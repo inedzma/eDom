bf5bf1ed2529cda916bb7b1147cd75441a3ecad7
Nedžma Imamović <nedzma.imamovic.23@size.ba>
2026-09-08 13:18:48 +0200

    deleting aiven password

--- edom-desktop/src/main/java/util/DBConnection.java
+++ edom-desktop/src/main/java/util/DBConnection.java
@@ -11,9 +11,9 @@ public class DBConnection {
     // 3. Promijenili smo parametar za SSL da odgovara Java driveru (sslMode=REQUIRED)
     private static final String URL = "jdbc:mysql://mysql-10b41bc7-size-e1bf.h.aivencloud.com:28740/edom?sslMode=REQUIRED";
 
-    private static final String USERNAME = "avnadmin";
+    private static final String USERNAME = "nestoLijevo";// this is a placeholder username, replace it with your actual username
 
-    private static final String PASSWORD = "AVNS_FWFxZ8Aax8kMSB59RwS";
+    private static final String PASSWORD = "nestoLijevo123";// this is a placeholder password, replace it with your actual password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
